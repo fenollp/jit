2f2a003cd00ffae384d64aa741444461dc5dff6c
Pierre Fenoll <pierrefenoll@gmail.com>
2018-08-23 22:27:44 +0200

    so BIFs including erlang:apply/3 cannot be traced...

--- src/jit_app.erl
+++ src/jit_app.erl
@@ -1,26 +1,33 @@
-%%%-------------------------------------------------------------------
-%% @doc jit public API
-%% @end
-%%%-------------------------------------------------------------------
-
 -module(jit_app).
-
 -behaviour(application).
 
 %% Application callbacks
 -export([start/2, stop/1]).
 
-%%====================================================================
-%% API
-%%====================================================================
+-export([watch/3, watch/0, traced/1, some_fun/0]).
 
 start(_StartType, _StartArgs) ->
     jit_sup:start_link().
 
-%%--------------------------------------------------------------------
 stop(_State) ->
     ok.
 
-%%====================================================================
-%% Internal functions
-%%====================================================================
+
+%% watch(M, F, A) when apply(erlang, is_integer, [A]) == true ->
+
+%% http://erlang.org/pipermail/erlang-questions/2016-September/090091.html
+%% > It is not possible to trace on what is called "guard bifs", i.e. functions
+%%   in the erlang module that can be used in guards.
+
+watch(M, F, A) ->
+    recon_trace:calls({M, F, A}, 10).
+
+some_fun() ->
+    ok.
+
+traced(M) ->
+    apply(M, some_fun, []).
+
+watch() ->
+    %% watch(erlang, apply, 3).
+    watch(?MODULE, traced, 1).
